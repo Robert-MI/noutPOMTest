70bc1d7f4aada01fd4dd80b4b026407ce133b39e
Robert-MI <mirzoyan.rob@gmail.com>
2024-04-25 00:18:21 +0400

    Initial commit

---
+++ src/main/java/constants/LocatorConstants.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class Locators {
+    public static final String NOUT_CLASS_NAME = "ammenu-logo";
+    public static final String ELECTRONICS_ID = "ui-id-1161";
+    public static final String LINK_LIST_CLASS = "ui-menu";
+    public static final String TV_ID = "ui-id-1004";
+    public static final String LINK_LIST_TV_CLASS = "product-items";
+
+}
---
+++ src/test/java/TVPageTest.java
@@ -0,0 +1,2 @@
+public class TVPageTest {
+}
